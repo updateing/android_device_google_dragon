284398255d6731e8cc5a0c96620f09add7287773
Hamster Tian <haotia@gmail.com>
2019-01-06 11:44:30 +0800

    dragon: add a custom camera provider service
    Dragon's camera HAL wants to access sensorservice directly
    but that's on a different binder. We have no further updates
    so the only way to get it working is to move the camera HAL
    to the same binder as sensorservice.
    
    This is a security risk and breaks Treble - so Google :-)

--- device.mk
+++ device.mk
@@ -129,7 +129,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.4-service.dragon \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     libshim_camera
