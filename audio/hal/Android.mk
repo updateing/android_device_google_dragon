f5421be8df53253e91d85f3a76c5d78c33ec294b
Hamster Tian <haotia@gmail.com>
2019-10-02 21:54:28 +0800

    dragon: use proper NEON instructions and clang to build audio HAL
    clang does not accept cp10 register in mrc/mcr instructions, and
    gcc could not link the binary in Q tree.
    Replace them with proper fmrx/fmxr to fix build with clang, and switch
    to it.
    
    This is taken from kernel arch/arm/vfp/vfpinstr.h

--- audio/hal/Android.mk
+++ audio/hal/Android.mk
@@ -48,7 +48,7 @@ LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_TAGS := optional
 
 # b/26236653, dsp_util.c inline assembly code does not compile with llvm yet.
-LOCAL_CLANG_CFLAGS += -no-integrated-as
+#LOCAL_CLANG_CFLAGS += -no-integrated-as
 
 LOCAL_VENDOR_MODULE := true
 
